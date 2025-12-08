ecf8ed28ad24fb0cc64adcfc7dce3b218e63504a
Rachee <git@rachee.dev>
2025-12-08 06:25:58 +0100

    formatting

--- src/lib/stores/useServerStore.tsx
+++ src/lib/stores/useServerStore.tsx
@@ -1,55 +1,57 @@
 import type {
-    ActivityId,
-    GroupDefinitions,
-    Player,
-    ServerState,
+	ActivityId,
+	GroupDefinitions,
+	Player,
+	ServerState,
 } from "@mikiwikipolvoron/wilco-lib/data";
 import { create } from "zustand";
 
 interface ServerStore extends ServerState {
-    // Server-side values
-    currentActivity: ActivityId;
-    players: Record<string, Player>;
-    groups?: GroupDefinitions;
-    connected: boolean;
+	// Server-side values
+	currentActivity: ActivityId;
+	players: Record<string, Player>;
+	groups?: GroupDefinitions;
+	connected: boolean;
 
-    // Client-side values, updated and tracked by us
-    nickname?: string;
-    registered: boolean;
-    assignedGroup?: string;
+	// Client-side values, updated and tracked by us
+	nickname?: string;
+	registered: boolean;
+	assignedGroup?: string;
 
-    _handlePlayerJoined: (player: Player) => void;
-    _handlePlayerLeft: (playerId: string) => void;
-    _handleActivityStarted: (activity: ActivityId) => void;
-    _handleGroupsUpdated: (groups: GroupDefinitions) => void;
-    _setConnected: (connected: boolean) => void;
+	_handlePlayerJoined: (player: Player) => void;
+	_handlePlayerLeft: (playerId: string) => void;
+	_handleActivityStarted: (activity: ActivityId) => void;
+	_handleGroupsUpdated: (groups: GroupDefinitions) => void;
+	_setConnected: (connected: boolean) => void;
 }
 
 const initialState = {
-    currentActivity: "start" as ActivityId,
-    players: {},
-    groups: undefined,
-    connected: false,
-    nickname: undefined,
-    registered: false,
-    assignedGroup: undefined,
+	currentActivity: "start" as ActivityId,
+	players: {},
+	groups: undefined,
+	connected: false,
+	nickname: undefined,
+	registered: false,
+	assignedGroup: undefined,
 };
 
 export const useServerStore = create<ServerStore>((set) => ({
-    ...initialState,
+	...initialState,
 
-    _handlePlayerJoined: (player) => set((state) => ({
-        players: { ...state.players, [player.id]: player },
-    })),
+	_handlePlayerJoined: (player) =>
+		set((state) => ({
+			players: { ...state.players, [player.id]: player },
+		})),
 
-    _handlePlayerLeft: (playerId) => set((state) => {
-        const { [playerId]: _removed, ...players } = state.players;
-        return { players };
-    }),
+	_handlePlayerLeft: (playerId) =>
+		set((state) => {
+			const { [playerId]: _removed, ...players } = state.players;
+			return { players };
+		}),
 
-    _handleActivityStarted: (activity) => set({ currentActivity: activity }),
+	_handleActivityStarted: (activity) => set({ currentActivity: activity }),
 
-    _handleGroupsUpdated: (groups) => set({ groups }),
+	_handleGroupsUpdated: (groups) => set({ groups }),
 
-    _setConnected: (connected) => set({ connected }),
-}))
+	_setConnected: (connected) => set({ connected }),
+}));
