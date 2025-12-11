68f8ca3c2a4cd82e33802b664b08a0e93624cd3d
Rachee <git@rachee.dev>
2025-12-11 14:03:17 +0100

    fix image target issues

--- src/screens/ARScreen.tsx
+++ src/screens/ARScreen.tsx
@@ -64,7 +64,7 @@ function AnchoringPhase() {
 				<img
 					src="/target.png"
 					alt="AR Calibration Target"
-					className="max-w-2/5 max-h-2/5 object-contain"
+					className="max-w-xl max-h-96 object-contain mx-auto"
 				/>
 				<p className="text-2xl mt-8 opacity-70">
 					Activity starts in 30 seconds...
